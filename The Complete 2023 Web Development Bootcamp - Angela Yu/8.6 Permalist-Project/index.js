499fdb3b05afc0884cba865f471872e6a32617f9
Neda <neda.alipour39@gmail.com>
2025-12-07 20:43:33 +0400

    Update Permalist Project - Add ORDER BY id to getAllItems function - Add try catch

---
+++ The Complete 2023 Web Development Bootcamp - Angela Yu/8.6 Permalist-Project/index.js
@@ -0,0 +1,96 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "Woody25!",
+  port: 5432,
+});
+db.connect();
+
+let items = [];
+
+async function getAllItems() {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC")
+  return result.rows
+}
+
+async function addItem(title) {
+  const result = await db.query("INSERT INTO items (title) VALUES ($1);", 
+    [title]
+  )
+  return result.rows[0]
+}
+
+async function updateItem(id, title) {
+  const result = await db.query("UPDATE items SET title = ($1) WHERE id = ($2)", 
+    [title, id]
+  )
+  return result.rows
+}
+
+async function deleteItem(id) {
+  const result = await db.query("DELETE FROM items WHERE id = ($1)", 
+    [id]
+  )
+  return result.rows
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const items = await getAllItems()
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch(err) {
+    console.log(err)
+  }
+});
+
+app.post("/add", async (req, res) => {
+  try {
+    const title = req.body.newItem;
+    const new_item = await addItem(title)
+    items = getAllItems()
+    res.redirect("/");
+  } catch(err) {
+    console.log(err)
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  try {
+    const item_id = req.body.updatedItemId;
+    const title = req.body.updatedItemTitle;
+    const result = await updateItem(item_id, title)
+    items = getAllItems()
+    res.redirect("/");
+  } catch(err) {
+    console.log(err)
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  try {
+    const item_id = req.body.deleteItemId;
+    const result = await deleteItem(item_id)
+    items = getAllItems()
+    res.redirect("/");
+  } catch(err) {
+    console.log(err)
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
