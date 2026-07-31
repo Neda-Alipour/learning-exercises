0f62803cc9589bebaf96e9527b37607b7d719f99
Neda Alipour <neda.alipour.dev@gmail.com>
2026-07-31 21:44:29 +0400

    Implement seller authentication with JWT token generation and login endpoint

--- FastAPI/Intermediate FastAPI/Main Project/app/config.py
+++ FastAPI/Intermediate FastAPI/Main Project/app/config.py
@@ -1,9 +1,21 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
+_base_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True, extra="ignore")
+
 class DatabaseSetting(BaseSettings):
     DATABASE_URL: str
 
     # This configures how the settings are loaded
-    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True, extra="ignore")
+    model_config = _base_config
 
-setting = DatabaseSetting()
+
+class SecuritySettings(BaseSettings):
+
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+
+    model_config = _base_config
+
+db_setting = DatabaseSetting()
+security_settings = SecuritySettings()
